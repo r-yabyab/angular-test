3f55303ebd9de9d4c1fb556fd6fce0a997038fc7
Scruffy pc <cayabyabrr@yahoo.com>
2024-12-12 15:21:07 -0800

    homecomponent works

---
+++ src/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "homeSel",
+    templateUrl: "./home.component.html"
+})
+export class homeComponent implements OnInit {
+    constructor() {}
+    ngOnInit(): void {
+        
+    }
+    thing = "hi"
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { homeComponent } from './home/home.component';
 
-bootstrapApplication(AppComponent, appConfig)
+// bootstrapApplication(AppComponent, appConfig)
+// bootstrapApplication(AppComponent)
+bootstrapApplication(homeComponent)
   .catch((err) => console.error(err));
